1c487d0f12180ce494d171d37374793cfd0c0ce1
Leticijak <mnogomijeheavy@gmail.com>
2022-01-10 10:47:03 +0100

    Initializing...

---
+++ src/routes/about.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<div class="about">
+	<h2>About Us</h2>
+	<p>
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dicta minus ratione optio cumque
+		deserunt amet hic nulla obcaecati expedita mollitia assumenda harum quos minima quia ipsum,
+		inventore velit quae voluptatem veniam enim maiores! Eum dicta doloremque sapiente natus
+		deleniti blanditiis laborum, fuga harum, voluptatibus deserunt, obcaecati corporis adipisci
+		mollitia.
+	</p>
+</div>
+
+<style>
+	.about {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,29 @@
+<script>
+	let title = 'Welcome to Svelte Bane';
+	const updateTitle = () => {
+		title = 'Something completely new';
+	};
+</script>
+
+<div class="index">
+	<h2>{title}</h2>
+	<p>
+		Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe vel doloremque asperiores
+		eligendi reiciendis magnam nihil! Quos nam possimus adipisci placeat quibusdam corporis natus
+		nisi voluptatem, dolor dicta optio, distinctio assumenda odit nostrum beatae omnis mollitia
+		quaerat repudiandae dolorem est!
+	</p>
+	<button on:click={updateTitle}>Change the title</button>
+	<br />
+	<!-- <input type="text" bind:value={title} /> -->
+	<br />
+	<a href="/about">Go to about page</a>
+</div>
+
+<style>
+	.index {
+		text-align: center;
+		display: block;
+		margin: 20px auto;
+	}
+</style>
